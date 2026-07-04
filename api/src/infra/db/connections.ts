5a3105f90a0b6104318297e84c4324cb8fbdf73b
Yasmina Andrade <yasmina.andrade@yahoo.com.br>
2026-07-03 23:08:58 -0400

    feat: configure database connection and repository structure

--- api/src/infra/db/connections.ts
+++ api/src/infra/db/connections.ts
@@ -2,7 +2,8 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 import { env } from "../../env";
+import * as schema from "./schema";
 
 const client = postgres(env.DATABASE_URL);
 
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
---
+++ api/src/infra/repositories/drizzle-link-repository.ts
@@ -0,0 +1,36 @@
+import { eq } from "drizzle-orm";
+
+import { Link } from "@/app/entities/Link";
+import {
+  CreateLinkDTO,
+  LinkRepository,
+} from "@/app/repositories/LinkRepository";
+
+import { db } from "../db/connections";
+import { links } from "../db/schema/links";
+
+export class DrizzleLinkRepository implements LinkRepository {
+  async create(data: CreateLinkDTO): Promise<Link> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findById(id: string): Promise<Link | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findByShortCode(shortCode: string): Promise<Link | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findAll(): Promise<Link[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  async delete(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async incrementAccessCount(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
