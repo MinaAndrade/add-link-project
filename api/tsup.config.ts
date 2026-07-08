c66dc8dfa23acb97014a5dfa98d1308000ec1212
Yasmina Andrade <yasmina.andrade@yahoo.com.br>
2026-07-08 14:33:09 -0400

    fix(backend): build only api server entrypoint

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/**/*.ts'],
+  entry: ['src/infra/http/server.ts'],
   clean: true,
   format: 'esm',
   outDir: 'dist',
